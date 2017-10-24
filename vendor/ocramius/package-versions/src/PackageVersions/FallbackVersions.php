d6bbe6a61ef1c0a97a114a23b650c7d37b3b77df
Ondrej Mirtes <ondrej@mirtes.cz>
2017-10-24 05:56:11 +0200

    rename

--- vendor/ocramius/package-versions/src/PackageVersions/FallbackVersions.php
+++ vendor/ocramius/package-versions/src/PackageVersions/FallbackVersions.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PackageVersions;
+namespace PHPStanVendor\PackageVersions;
 
 /**
  * @internal
--- vendor/ocramius/package-versions/src/PackageVersions/Installer.php
+++ vendor/ocramius/package-versions/src/PackageVersions/Installer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace PackageVersions;
+namespace PHPStanVendor\PackageVersions;
 
 use Composer\Composer;
 use Composer\Config;
